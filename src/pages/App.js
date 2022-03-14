f14ba7b7c9e0b4f0ff4aa331250c6c2fc2367008
Nathan Rodet <rodetnathan.contact@gmail.com>
2022-03-14 16:53:06 +0100

    initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,24 @@
+import '../styles/app.css';
+
+function App() {
+  return (
+    //<img src={logo} className="App-logo" alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/pages/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
